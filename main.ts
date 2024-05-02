208750e7c48c8de7846fd9685dc57995aaebf274
Yongwook Choi <flow@hrmm.xyz>
2024-05-02 23:53:20 +0900

    Add prescript to autogenerate tailwind config

--- main.ts
+++ main.ts
@@ -9,5 +9,8 @@ import "@std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
+import { writeTailwindConfig } from "~/prestart.ts";
+
+await writeTailwindConfig();
 
 await start(manifest, config);
