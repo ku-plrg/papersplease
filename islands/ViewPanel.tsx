8d5eaa1def3285b9d7dc32cba1ea309ee9b35cd6
tmdghks <tmdghks12@korea.ac.kr>
2024-08-19 18:06:35 +0900

    feat: add copy code button in view pannel
    Co-authored-by: Yongwook Choi <flow@hrmm.xyz>

--- islands/ViewPanel.tsx
+++ islands/ViewPanel.tsx
@@ -30,11 +30,30 @@ export default function ViewPanel({ filename, lang }: PanelProps) {
   }
   return (
     <Card>
-      <div>{filename}</div>
+      <div class="flex">
+        <div class="min-w-24">{filename}</div>
+        <button
+          onClick={() =>
+            navigator.clipboard.writeText(extractCodeFromHtml(body.value))}
+        >
+          Copy Code
+        </button>
+      </div>
       <div
         class="overflow-auto"
         dangerouslySetInnerHTML={{ __html: body.value }}
       />
     </Card>
   );
+}
+
+function extractCodeFromHtml(htmlString: string): string {
+  // Create a DOM parser
+  const parser = new DOMParser();
+  // Parse the HTML string
+  const doc = parser.parseFromString(htmlString, "text/html");
+  // Select the code element within the pre element
+  const codeElement = doc.querySelector("pre code");
+  // Extract and return the text content
+  return codeElement?.textContent ?? "";
 }
